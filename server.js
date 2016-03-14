9dd4fa6091996ae31d4a3b602c290f4e1106dceb
Micah Wells <micahp0506@gmail.com>
2016-03-14 16:05:26 -0500

    React boiler plate running with node server

--- server.js
+++ server.js
@@ -3,3 +3,16 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Defining PORT, 3000 or process.env
+const PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log(`Node.js server has started. Listening on port ${PORT}`);
+  });
