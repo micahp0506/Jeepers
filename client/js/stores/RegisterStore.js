167b04177112e27cc55b43dd4faabfb521a30681
Micah Wells <micahp0506@gmail.com>
2016-03-16 11:39:10 -0500

    Added comments to RegisterStore

---
+++ client/js/stores/RegisterStore.js
@@ -0,0 +1,61 @@
+'use strict';
+
+
+import alt from '../alt';
+import RegisterActions from '../actions/RegisterActions';
+
+// Creating register store constructor
+class RegisterStore {
+  constructor() {
+    this.bindActions(RegisterActions);
+    this.email = '';
+    this.helpBlock = '';
+    this.emailValidationState = '';
+    this.passwordValidationState = '';
+  }
+
+  // Handling the registration of new user
+  onregisterSuccess(successMessage) {
+    this.emailValidationState = 'has-success';
+    this.helpBlock = successMessage;
+  }
+
+  // Handling the failure to register of new user
+  onregisterFail(errorMessage) {
+    this.emailValidationState = 'has-error';
+    this.helpBlock = errorMessage;
+  }
+
+  // Binding provided email
+  onnewEmail(event) {
+    this.email = event.target.value;
+    this.emailValidationState = '';
+    this.helpBlock = '';
+  }
+
+  // Binding provided hashed password
+  onnewPassword(event) {
+    this.password = event.target.value;
+    this.passwordValidationState = '';
+  }
+
+  // Handling no email provided by user
+  onnoEmail() {
+    this.emailValidationState = 'has-error';
+    this.helpBlock = 'Please enter an email.';
+  }
+
+  // Handling no password provided by user
+  onnoPassword() {
+    this.passwordValidationState = 'has-error';
+    this.helpBlock = 'Please enter a password';
+  }
+
+  // Handling the passwords not matching
+  ondoesNotMatch() {
+    this.passwordValidationState = 'has-error';
+    this.helpBlock = 'Passwords do not match.';
+  }
+}
+
+export default alt.createStore(AddCharacterStore);
